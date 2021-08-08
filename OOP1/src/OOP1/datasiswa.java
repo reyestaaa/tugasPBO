ab1f251ac8e5e7647ca75fadc4ee42c20e046eb5
reyestaaa <muhammadadrianreyesta9d@gmail.com>
2021-08-08 21:23:40 +0700

    first commit

---
+++ OOP1/src/OOP1/datasiswa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OOP1;
+
+/**
+ *
+ * @author User
+ */
+public class datasiswa {
+    //ATRIBUT
+    String nama = "Adrian";
+    int umur = 17;
+    double tinggi = 180.4;
+}
---
+++ OOP1/src/OOP1/siswa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OOP1;
+
+/**
+ *
+ * @author User
+ */
+
+//CLASS
+public class siswa {
+    public static void main(String[] args){
+        //OBJECT
+        datasiswa objectSiswa = new datasiswa();
+        System.out.println("NAMA: "+objectSiswa.nama);
+        System.out.println("UMUR: "+objectSiswa.umur);
+        System.out.println("TINGGI: "+objectSiswa.tinggi);
+    }
+}
